130c1ae692df47746ab5300b287edace4834874d
kdohyeong <do5568@naver.com>
2019-12-28 21:56:00 +0900

    [Bug Fix]

--- test1.py
+++ test1.py
@@ -1,9 +1,19 @@
 def add(a,b) :
     return a + b 
 
+print (add(5, 9))
 
 def sub(a ,b): 
     return a - b
 
+print (sub(5, 9))
+
+
+print sub(9, 9)
+
 def mul(a,b):
     return a * b
+
+print (mul(5, 10))
+
+
